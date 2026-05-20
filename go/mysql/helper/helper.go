5972fc6b31e32e657c1d26dd20062d70fb46a738
Kakeru Sato <sato.kakeru3@gmail.com>
2026-05-20 16:39:14 +0900

    under dev

--- go/mysql/helper/helper.go
+++ go/mysql/helper/helper.go
@@ -4,6 +4,7 @@
 package helper
 
 import (
+    "context"
     "database/sql"
     "fmt"
     "strconv"
@@ -44,8 +45,11 @@ type UpdateField[T any] struct {
 
 type Executor interface {
     Exec(query string, args ...any) (sql.Result, error)
+    ExecContext(ctx context.Context, query string, args ...any) (sql.Result, error)
     Query(query string, args ...any) (*sql.Rows, error)
+    QueryContext(ctx context.Context, query string, args ...any) (*sql.Rows, error)
     QueryRow(query string, args ...any) *sql.Row
+    QueryRowContext(ctx context.Context, query string, args ...any) *sql.Row
 }
 
 
